cb1afc5655e59074468064311177af1788922a3e
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-05-16 14:51:19 +0200

    header static

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,101 @@
+<template>
+  <header >
+    <div class="header-container">
+     
+      <div class="top-header">
+        <div class="logo">
+          <img src="../assets/images/gym_logo_1x.png" alt="">
+        </div>
+        <nav-bar />
+      </div>
+
+      <div class="container text-center">
+        <h1>No pain no gain</h1>
+        <p>Motivation is what gets you started. Habit is what keeps you going.</p>
+        <button>Join us today</button>
+      </div>
+      
+    </div>
+
+  </header>
+  
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+
+
+export default {
+  components: { 
+    NavBar,
+
+    },
+  name: 'AppHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../style/vars.scss';
+
+header {
+  background: $backgroundBlack;
+  height: 100vh;
+  color: $textWhite;
+  background-image: url('../assets/images/home-banner.jpg');
+  background-repeat: no-repeat;
+  background-position: top;
+ 
+}
+
+.header-container {
+  height: 100%;
+  
+}
+
+.top-header {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 200px;
+  padding-top: 90px;
+  align-items: baseline;
+}
+
+header .container {
+  height: calc(100% - 133px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  bottom: 145px;
+  h1, button {
+    text-transform: uppercase;
+  }
+}
+
+h1 {
+  font-size: 115px;
+  font-weight: 700;
+}
+p {
+  font-size: 24px;
+}
+
+ button {
+  background: none;
+  color: $textWhite;
+  border: 1px solid $textWhite;
+  padding: 10px 25px;
+}
+
+  
+
+.debug {
+  border: 1px solid black;
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="nav-bar">
+        <ul>
+            <li v-for="link in links" :key="link">{{link}}</li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data(){
+      return {
+          links: [
+              'Home',
+              'About Us',
+              'Facilities',
+              'Membership',
+              'Testimonials',
+              'Blog'
+          ]
+      }
+  },
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.nav-bar {
+    display: flex;
+    align-items: center;
+    ul {
+        display: flex;
+        li {
+        margin: 0 12px;
+        text-transform: uppercase;
+        list-style-type: none;
+        }
+    }
+}
+
+  
+</style>
